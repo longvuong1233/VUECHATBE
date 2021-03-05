f12ef6cdad89c74ae4dfcd54fa1706054d019274
longvuong1233 <hadinhcpr1233@gmail.com>
2021-03-05 21:17:51 +0700

    first commit

--- mailer/config.js
+++ mailer/config.js
@@ -1,10 +1,10 @@
-const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer");
 
 module.exports = nodemailer.createTransport({
-    service: "gmail",
-    port: 465,
-    auth: {
-        user: "appchathadinh@gmail.com",
-        pass: "appchathadinh1233"
-    }
-})
+  service: "gmail",
+  port: 465,
+  auth: {
+    user: "appchathadinh@gmail.com",
+    pass: "appchathadinh1233",
+  },
+});
